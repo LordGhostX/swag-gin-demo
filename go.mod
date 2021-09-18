a0efd2af1895142cf06adc88fbb67605e26daa69
LordGhostX <solomonghost9@gmail.com>
2021-09-18 06:03:26 +0100

    initial commit

---
+++ go.mod
@@ -0,0 +1,18 @@
+module swag-gin-demo
+
+go 1.16
+
+require (
+	github.com/gin-gonic/gin v1.7.4 // indirect
+	github.com/go-playground/validator/v10 v10.9.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/ugorji/go v1.2.6 // indirect
+	golang.org/x/crypto v0.0.0-20210915214749-c084706c2272 // indirect
+	golang.org/x/sys v0.0.0-20210917161153-d61c044b1678 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+// todo represents data about a task in the todo list
+type todo struct {
+	ID string `json:"id"`
+	Task string `json:"task"`
+}
+
+// Todos slice to seed todo list data
+var todoList = []todo{
+	{"1", "Learn Go"},
+	{"2", "Build an API with Go"},
+	{"3", "Document the API with Swag"},
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/todo", getAllTodos)
+	router.GET("/todo/:id", getTodoByID)
+	router.POST("/todo", createTodo)
+	router.DELETE("/todo/:id", deleteTodo)
+
+	router.Run()
+}
+
+func getAllTodos(c *gin.Context) {
+	c.JSON(http.StatusOK, todoList)
+}
+
+func getTodoByID(c *gin.Context) {
+	ID := c.Param("id")
+
+	// loop through todoList and return item with matching ID
+	for _, todo := range todoList {
+		if todo.ID == ID {
+			c.JSON(http.StatusOK, todo)
+			return
+		}
+	}
+
+	// return error message if todo is not found
+	c.JSON(http.StatusNotFound, gin.H{
+		"message": "todo not found",
+	})
+}
+
+func createTodo(c *gin.Context) {
+	var newTodo todo
+
+	// bind the received JSON data to newTodo
+	if err := c.BindJSON(&newTodo); err != nil {
+		return
+	}
+
+	// add the new todo item to todoList
+	todoList = append(todoList, newTodo)
+	c.JSON(http.StatusCreated, newTodo)
+}
+
+func deleteTodo(c *gin.Context) {
+	ID := c.Param("id")
+
+	// loop through todoList and delete item with matching ID
+	for index, todo := range todoList {
+		if todo.ID == ID {
+			todoList = append(todoList[:index], todoList[index+1:]...)
+			c.JSON(http.StatusOK, gin.H{
+				"message": "successfully deleted todo",
+			})
+			return
+		}
+	}
+
+	// return error message if todo is not found
+	c.JSON(http.StatusNotFound, gin.H{
+		"message": "todo not found",
+	})
+}
